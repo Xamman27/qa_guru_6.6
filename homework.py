d891d37453ee7715a37a1f05f542f0a4a2c11ec8
Xamman27 <xamman27@gmail.com>
2023-06-26 11:36:19 +0400

    rework 1 task

--- homework.py
+++ homework.py
@@ -5,7 +5,7 @@ def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
-    current_time = time(hour=23)
+    current_time = time(hour=4)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
     is_dark_theme = automation_enable_dark_theme(current_time)
@@ -13,7 +13,7 @@ def test_dark_theme_by_time():
 
 
 def automation_enable_dark_theme(current_time):
-    if time(hour=22) <= current_time > time(hour=6):
+    if time(hour=22) <= current_time or current_time < time(hour=6):
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -27,17 +27,18 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+    current_time = time(hour=4)
+    dark_theme_enabled_by_user = None
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = automation_enable_dark_theme_and_user_chooise(current_time,dark_theme_enabled_by_user)
+    is_dark_theme = automation_enable_dark_theme_and_user_chooise(current_time, dark_theme_enabled_by_user)
     assert is_dark_theme is True
 
 
-def automation_enable_dark_theme_and_user_chooise(current_time,dark_theme_enabled_by_user):
-    if time(hour=22) <= current_time > time(hour=6) and dark_theme_enabled_by_user is None:
+def automation_enable_dark_theme_and_user_chooise(current_time, dark_theme_enabled_by_user):
+    sys_dark_theme = automation_enable_dark_theme(current_time)
+    if sys_dark_theme and dark_theme_enabled_by_user is None:
         is_dark_theme = True
     elif dark_theme_enabled_by_user is True:
         is_dark_theme = True
