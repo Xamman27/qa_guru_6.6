946467109dc9e50d1528548d17f06378f812ee79
Xamman27 <xamman27@gmail.com>
2023-06-26 11:55:15 +0400

    rework 4 task

--- homework.py
+++ homework.py
@@ -90,29 +90,24 @@ def test_readable_function():
 
 def open_browser(browser_name):
     actual_result = get_name_func(open_browser, browser_name)
+    print('\n', actual_result)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = get_name_func(go_to_companyname_homepage, page_url)
+    print(actual_result)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = get_name_func(find_registration_button_on_login_page,page_url, button_text)
+    print(actual_result)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
 
+
 def get_name_func(func_name, *func_arg):
-    upp_flag = True
-    new = []
-    for i in str(func_name.__name__):
-        if upp_flag:
-            new.append(str(i).upper())
-            upp_flag = False
-        elif str(i) == "_":
-            new.append(str(' '))
-            upp_flag = True
-        else:
-            new.append(str(i))
-    name = ''.join(new) + ' ' + '[' + ', '.join(func_arg) + ']'
-    return name
+    func_name = func_name.__name__.replace('_', ' ').title()
+    arg_name = ", ".join([*func_arg])
+
+    return (f'{func_name} [{arg_name}]')
